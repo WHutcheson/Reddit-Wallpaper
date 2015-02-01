8ecb01cb53d21c575474002487e5c24d998c7617
William <hintofbasil@github.com>
2015-02-01 18:12:52 +0000

    Added loop to wait for window session to boot

--- reddit-wallpaper.py
+++ reddit-wallpaper.py
@@ -2,12 +2,15 @@
 
 import praw
 import requests
+import subprocess
+import shlex
+import time
 from os import path
-from subprocess import call
 
-SAVE_LOCATION = path.expanduser('~/.background_image')
+SAVE_LOCATION = path.expanduser('/home/will/.wallpaper')
 SUBREDDIT = 'imaginarylandscapes'
-SET_COMMAND = 'feh --bg-fill {}'
+SET_COMMAND = 'gsettings set org.gnome.desktop.background picture-uri file://{}'
+WINDOW_SESSION_ERROR = 'dconf-WARNING'
 
 def is_image(url):
     r = requests.head(url)
@@ -16,7 +19,6 @@ def is_image(url):
     return False
 
 if __name__=='__main__':
-    print SAVE_LOCATION
     r = praw.Reddit(user_agent='reddit-wallpaper')
     subreddit = r.get_subreddit(SUBREDDIT)
 
@@ -26,5 +28,18 @@ if __name__=='__main__':
             with open(SAVE_LOCATION, 'wb') as f:
                 for chunk in r.iter_content():
                     f.write(chunk)
-            call(SET_COMMAND.format(SAVE_LOCATION), shell=True)
+            run = 1000
+            while(run):
+                sp = subprocess.Popen(
+                        #['ls']
+                        shlex.split(SET_COMMAND.format(SAVE_LOCATION)),
+                        #shell=True
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                        )
+                if not WINDOW_SESSION_ERROR in sp.communicate()[1]:
+                    run=False
+                else:
+                    time.sleep(0.1)
+                    run-=1
             break
